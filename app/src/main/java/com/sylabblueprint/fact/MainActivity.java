2eb4d26635440dde1055e873a4d6fb776ea5074d
Abraham Sylvester <mrabblessedslyst4christ099@gmail.com>
2019-11-27 16:56:13 +0200

    My first commit

--- app/src/main/java/com/sylabblueprint/fact/MainActivity.java
+++ app/src/main/java/com/sylabblueprint/fact/MainActivity.java
@@ -1,6 +1,5 @@
-package com.mainor.fact;
+package com.sylabblueprint.fact;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,42 +9,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText;
-    Button button;
+    EditText textField;
+    Button btn;
     TextView textView;
+
+    private double fact(double val) {
+        if (val < 2){
+            return 1;
+        }
+        else {
+            return val * fact(val - 1);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = (EditText) findViewById(R.id.editText);
+        textField = (EditText) findViewById(R.id.textField);
         textView = (TextView) findViewById(R.id.textView);
-        button = (Button) findViewById(R.id.button);
+        btn = (Button) findViewById(R.id.btn);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (TextUtils.isEmpty(editText.getText().toString())){
-                    editText.setError("Please enter a number and try Again!");
-                    editText.setFocusable(true);
+                    int val = Integer.parseInt(textField.getText().toString());
+                    textView.setText(" " + String.format(Locale.getDefault(), "%.0f", fact(val)));
                 }
-                else {
-                    int num = Integer.parseInt(editText.getText().toString());
-                    textView.setText(" " + String.format(Locale.getDefault(), "%.0f", factorial(num)));
-                }
-            }
-
 
         });
 
-    }
-
-    private double factorial(double num) {
-        if (num < 2){
-            return 1;
-        }
-        else {
-            return num * factorial(num - 1);
-        }
     }
 }
